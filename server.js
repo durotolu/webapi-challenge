60f9952fad7d1499274c5b2dcc85857d27a05c6e
Modurotolu Olokode <durolawk@gmail.com>
2019-11-01 10:28:22 +0100

    built endpoints for project routes

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const server = express();
 const cors = require('cors');
 const helmet = require('helmet');
 const projectsRouter = require('./routes/projectsRoutes');
-const actionsRoute = require('./routes/actionRoutes');
+//const actionsRoute = require('./routes/actionRoutes');
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
 server.use('/api/projects', projectsRouter);
-server.use('/api/actions', actionsRoute);
+//server.use('/api/actions', actionsRoute);
 
 module.exports = server;
